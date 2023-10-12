861c257878ba55d90040db08fb787ba68b87fa2b
Lemelinha <67035515+lemelinha@users.noreply.github.com>
2023-10-12 02:08:55 -0300

    mudança na animação do cabecalho

--- scripts/cabecalho.js
+++ scripts/cabecalho.js
@@ -1,25 +1,18 @@
 window.onscroll = function () {
     $("header").animate({ height: "100px" }, 400);
-    $(".container-header img").animate({
-        width: "100px",
-        height: "100px",
-        maxWidth: "100%"
-    }, 200);
     $(".container-header").animate({
         height: "100px",
-        width: "99vw",
-        maxWidth: "100vw",
         padding: "0"
     }, 300);
+
+    $("header #logo-cabecalho").transit({ "transform": "translate(0)" }, 300);
+
     $(".container-header h2").hide("fast");
+    $(".container-header img").hide("fast");
+
 
     if (document.getElementById("header").style.height != "100px") {
         this.scrollTo(0, 0);
     }
 
-    if (window.innerWidth < 420) {
-        $(".container-header img").hide("fast");
-    } else {
-        $(".container-header img").show("fast");
-    }
 };
